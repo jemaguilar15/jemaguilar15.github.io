e989ff2e4415c8676e75218c8eb803eae0a8d027
John <design.johnaguilar@gmail.com>
2020-08-11 14:39:34 +0800

    Pushing to production heroku

--- app.js
+++ app.js
@@ -2,21 +2,55 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require("body-parser");
+
+const nodemailer = require("nodemailer");
+const mg = require("nodemailer-mailgun-transport");
 
 app.use(express.static("public"));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
+app.use(express.json());
+
+const auth = {
+  auth: {
+    api_key: process.env.MG_API_KEY,
+    domain: process.env.MG_DOMAIN
+  }
+}
+
+app.post("/", (req, res) => {
+  const { emailAddress, emailSubject, emailMessage } = req.body;
+  console.log(req.body);
+  const nodemailerMailgun = nodemailer.createTransport(mg(auth));
+  nodemailerMailgun.sendMail({
+    from: `sender: <${emailAddress}>`,
+    to: "design.johnaguilar@gmail.com",
+    subject: emailSubject,
+    text: emailMessage
+  }, (err, data) => {
+    if (err) {
+      console.log(err);
+    } 
+    // else {
+    //   console.log("Message sent!");
+    // }
+  });
+  // res.json({ message: 'Message received!'});
+  
+  res.redirect("/");
+
+});
 // |---------------
 // | Get Routes
 // |---------------
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
-
-app.get('/portfolio', (req, res) => {
-    res.sendFile(__dirname + "/views/portfolio.html");
-})
 
 let port = process.env.PORT;
 if (port == null || port == "") {
--- public/js/animation.js
+++ public/js/animation.js
@@ -1,27 +1,81 @@
-$(".hb-menu").click(function(){
-    $(".nav-menu").slideToggle(300);
-    setTimeout(() => {
-        $(".bar-line").toggleClass("toggle-bgcolor");
-        $(".title").toggleClass("toggle-color").toggleClass("title-border");
-    }, 100);   
+
+// @TODO 
+// |-------------------
+// | Animating header nav menu
+// |-------------------
+// const burgerMenu = document.getElementsByClassName("hb-menu");
+$(window).on('beforeunload', function() {
+    //   $('body').hide();
+      $(window).scrollTop(0);
+});
+const nav = document.getElementById("nav-menu");
+const title = document.getElementById("john");
+const burgerMenu = document.querySelectorAll(".bar-line");
+
+function slideNav() {
+
+        for (i = 0; i < burgerMenu.length; i++) {
+            burgerMenu[i].classList.toggle("toggle-bgcolor");
+        }
+
+        title.classList.toggle("toggle-color");
+        title.classList.toggle("title-border");
+
+        if (nav.style.top !== "0px") {
+        
+            nav.style.top = "0";  
+        } else {
+            nav.style.top = "-300px"
+        }
+
+}
+
+function reset() {
+    document.getElementById("contact-form").reset();
+    alert("Message sent!");
+    // return false;
+}
+
+
+let prevScroll = window.pageYOffset;
+
+let portfolioImgAsset = document.getElementById("image-asset");
+
+$(document).ready(() => {
+    $(document).scroll(() => {
+        // console.log($(document).scrollTop());
+        let currentScroll = window.pageYOffset;
+
+        if ($(document).scrollTop() > 250) {
+            // console.log("Hello World");
+            if (prevScroll > currentScroll) {
+
+                if (nav.style.top === "0px") {
+                    for (i = 0; i < burgerMenu.length; i++) {
+                        burgerMenu[i].classList.toggle("toggle-bgcolor");
+                    }
+                    title.classList.toggle("toggle-color");
+                    title.classList.toggle("title-border");
+                    nav.style.top ="-300px";
+                }
+                defaultScroll();
+              } else {
+                userScrolled();
+              }
+              prevScroll = currentScroll;
+        }
+    });
 });
 
-// $(document).ready(() => {
-//     $(document).scroll(() => {
-//         if( $(document).scrollTop() === 0 || $(document).scrollTop() < 200) {
-//             defaultScroll();
-//         } else {
-//             userScrolled();
-//         }
-//     });
-// });
+const navbar = document.getElementById("header");
+function defaultScroll() {
+    return $(".grid-header").slideDown("fast");
+    // return navbar.style.visibility ="visible";
+}
 
-// function defaultScroll() {
-//     return $(".grid-header").slideDown("slow");
-// }
-
-// function userScrolled() {
-//     return $(".grid-header").slideUp("slow");
-// }
+function userScrolled() {
+    return $(".grid-header").slideUp("fast");
+    // return navbar.style.visibility ="hidden";
+}
 
 
